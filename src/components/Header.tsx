90bccab2643e3c14c535f20a46f281a5c98c2079
ntrglcrn <alexandrkarb@gmail.com>
2025-06-05 07:07:21 +0500

    Обновление компонентов и стилей: изменены размеры шрифтов в Header, добавлено свойство suppressHydrationWarning в RootLayout, изменены отступы в HeroSection. Обновлены файлы индекса и favicon.ico.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ const Header: React.FC = () => {
   return (
     <header className="w-full px-4 py-2 flex items-center justify-between bg-white">
       {/* Логотип - используем заглушку, так как это булевая операция в Figma */}
-      <div className="text-[#014d8c] text-[24px] font-absurdite">
+      <div className="text-[#014d8c] text-[16px] font-absurdite">
         {/* Здесь будет ваш логотип, например, SVG или изображение */}
         Karal.Design
       </div>
@@ -13,17 +13,17 @@ const Header: React.FC = () => {
       <nav>
         <ul className="flex items-center space-x-12">
           <li>
-            <Link href="/cases" className="text-[#014d8c] text-[18px] font-sf-pro-display-regular">
+            <Link href="/cases" className="text-[#014d8c] text-[16px] font-sf-pro-display-regular">
               Work
             </Link>
           </li>
           <li>
-            <Link href="/about" className="text-[#014d8c] text-[18px] font-sf-pro-display-regular">
+            <Link href="/about" className="text-[#014d8c] text-[16px] font-sf-pro-display-regular">
               About
             </Link>
           </li>
           <li>
-            <Link href="/contact" className="text-[#014d8c] text-[18px] font-sf-pro-display-regular">
+            <Link href="/contact" className="text-[#014d8c] text-[16px] font-sf-pro-display-regular">
               Contact
             </Link>
           </li>
